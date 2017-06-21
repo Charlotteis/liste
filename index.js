b5c708d5851b7bff624143bf5d0555e8feaa4997
System <none>
2017-06-21 17:42:10 +0000

    chore: initial project setup

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const choo = require('choo')
+const nameStore = require('./stores/nameStore')
+const main = require('./templates/main')
+
+const app = choo()
+
+if (process.env.NODE_ENV !== 'production') {
+  const log = require('choo-log')
+  app.use(log())
+}
+
+app.use(nameStore)
+
+app.route('/', main)
+
+app.mount('body')
