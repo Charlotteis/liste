ccaacfc85e06828ec933570512924a85e47800f1
Charlotte Spencer <charlottelaspencer@gmail.com>
2017-07-15 12:45:53 +0100

    feat: localStorage persistence

--- stores/todoStore.js
+++ stores/todoStore.js
@@ -1,9 +1,13 @@
+/* globals localStorage */
+
 'use strict'
 
 module.exports = (state, emitter) => {
-  state.todos = state.todos || []
+  const todos = localStorage.getItem('todos') ? localStorage.getItem('todos').split(',') : []
+  state.todos = todos || []
   emitter.on('addTodo', (todo) => {
     state.todos.push(todo)
+    localStorage.setItem('todos', state.todos)
     emitter.emit('render')
   })
 }
